f01b70ea1082299c3c78f2564f78d37dea2dd725
Frederic Hallein <Frederic.Hallein@axxes.com>
2024-10-17 16:11:17 +0200

    first commit CoinPartition

---
+++ CoinPartition.cs
@@ -0,0 +1,45 @@
+﻿namespace project_euler
+{
+    class CoinPartition
+    {
+
+        static int P_nr(int n, int r)
+        {
+
+            if (n < r)           { return 0; }
+            else if (n == r)     { return 1; }
+            else if (n == r + 1) { return 1; }
+            else if (r == 1)     { return 1; }
+            else if (r == 2) {
+                if (n % 2 == 0)  { return n / 2; }
+                else             { return (n - 1) / 2; }
+            }
+            
+            else {               
+                int p_nr = 0;
+                for (int k = 1; k <= r; k++)
+                {
+                    p_nr += P_nr(n - r, k);
+                }
+                return p_nr;
+            }
+        
+        }
+
+
+        static void Main(string[] args)
+        {
+            int n = 6;
+            int r = 3;
+
+            int p_nr = 0;
+            for (int k = 1; k <= r; k++)
+            {
+                p_nr += P_nr(n - r, k);
+            }
+        
+
+            Console.WriteLine("p(" + n + ", " + r + ") = " + p_nr);
+        }
+    }
+}
