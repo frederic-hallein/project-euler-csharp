e25054d04f622cb15fa6d6682c0c6985d497160e
Frederic Hallein <Frederic.Hallein@axxes.com>
2024-10-22 10:43:40 +0200

    added dictionary which improves speed and finds answer to problem

--- CoinPartitions.cs
+++ CoinPartitions.cs
@@ -16,90 +16,51 @@
 
     Find the least value of n for which p(n) is divisible by one million.
     */
+    
+    using System.Numerics;
 
     class CoinPartitions
     {
+        static readonly Dictionary<int, BigInteger> pValues = [];
 
-        static ulong P_nr(int n, int r)
+        // calculates the partition function p(n) using the recurrence relations from https://en.wikipedia.org/wiki/Partition_function_(number_theory).
+        static BigInteger P(int n)
         {
-            
-            if      (n < r)      { return 0; }
-            else if (n == r)     { return 1; }
-            else if (n == r + 1) { return 1; }
-            else if (r == 1)     { return 1; }
-            else if (r == 2) {
-                if (n % 2 == 0)  { return (ulong)(n / 2); }
-                else             { return (ulong)((n - 1) / 2); }
-            }
-            
-            else {               
-                ulong p_nr = 0;
-                for (int k = 1; k <= r; k++) { p_nr += P_nr(n - r, k); }
-                return p_nr;
-            }
-        
-        }
-
-
-
-
-        static ulong P(long n)
-        {
-
-            if      (n == 0) { return 1; }
+            if (pValues.TryGetValue(n, out BigInteger pValue)) { return pValue; }
+            else if (n == 0) { return 1; }
             else if (n < 0)  { return 0; }
             
-            ulong p = 0;
+            BigInteger p = 0;
             double lowerBound = - (Math.Sqrt(24 * n + 1) - 1) / 6;
             double upperBound =   (Math.Sqrt(24 * n + 1) + 1) / 6;
 
-
-
-            for (long k = (long)Math.Ceiling(lowerBound); k <= upperBound; k++) {
+            for (int k = (int)Math.Ceiling(lowerBound); k <= upperBound; k++) {
                 if (k != 0) {
-                    long nPar = n - k * (3 * k - 1) / 2;
-                    p += (ulong)(Math.Pow(-1, k + 1) * P(nPar));
+                    int nPar = n - k * (3 * k - 1) / 2;
+                    p += (BigInteger)Math.Pow(-1, k + 1) * P(nPar);
                 }
             }
-            return p;
-            
+
+            pValues.Add(n, p);
+            return p;   
         }
         
 
 
         static void Main(string[] args)
-        {
-            /*
+        {           
             int n = 0;
-            ulong p_n = 0;
-
-            // TODO : use Pentagonal number theorem to speed up calculations
-            bool leastValueIsFound = false;
-            while (!leastValueIsFound) {
-                if (n == 0) { p_n++; }
-                for (int r = 1; r <= n; r++) {
-                    ulong p_nr = 0;
-                    for (int k = 1; k <= r; k++) { p_nr += P_nr(n - r, k); }
-                    if (n == r) { p_nr++;}
-                    p_n += p_nr;
-                } 
-
-                Console.WriteLine($"p({n}) = {p_n}");
-                if (p_n % 1000000 == 0) { leastValueIsFound = true; } 
-                else { n++; p_n = 0; }
-                
-            }
-            */
-            
-
-            ulong n = 0;
-            ulong p;
+            BigInteger p;
+            int divisor = 1000000;
 
             bool leastValueIsFound = false;
             while (!leastValueIsFound) {
-                p = P((long)n); 
+                p = P(n); 
                 Console.WriteLine($"p({n}) = {p}");
-                if (p % 1000000 == 0) { leastValueIsFound = true; } 
+                if (p % divisor == 0) { 
+                    leastValueIsFound = true; 
+                    Console.WriteLine($"\nThe least value of n for which p(n) is divisible by {divisor} is n = {n}."); 
+                }
                 else { n++; }
                 
             }
--- obj/Debug/net8.0/project_euler.AssemblyInfo.cs
+++ obj/Debug/net8.0/project_euler.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project_euler")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b7c0fe50a4442e462e4ededcf443db99b121f3e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9a9df6c5bff5b83db6050ca391d2031145b6b72")]
 [assembly: System.Reflection.AssemblyProductAttribute("project_euler")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project_euler")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
